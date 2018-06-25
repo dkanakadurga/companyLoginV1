c7fe62899c4c2460906e17f55350405b766d0417
Kanaka Durga <dkanakadurga@gmail.com>
2018-06-25 13:38:49 +0100

    Adding for the first time

---
+++ src/js/authenticate.js
@@ -0,0 +1,83 @@
+
+
+
+function displayWithAjax() {
+    var obj = {};
+    obj.userName = $("#userName").val();
+    obj.password = $("#password").val();
+   var obj = JSON.stringify(obj);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:49980/api/Authentication/ValidateUser",
+        data: obj,
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8", 
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            if(data.ValidUser === true && data.Reason === "Validated"){
+            window.location.href = "../Html/myPage.html"
+            }
+            else {
+                $("#errorMsgText").text("Invalid user name or password");
+            }    
+            
+        },
+        error: function () {
+            alert("error");
+           
+        }
+    });
+}
+    
+function displayWithPromise(){
+    var promise1 = new Promise(function(resolve, reject) {
+          var obj = {};
+     obj.userName = $("#userName").val();
+     obj.password = $("#password").val();                       
+        axios.post("http://localhost:49980/api/Authentication/ValidateUser",  obj , {
+            headers: {
+                'content-Type': 'application/json;charset=UTF-8',
+              'Access-Control-Allow-Origin': 'localhost:8080',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Methods': 'DELETE, PUT, GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                
+            }
+        });
+      resolve(obj); 
+    })
+    promise1.then(value => {
+        console.log(value);
+        
+    }, reason => {
+        console.log(reason);
+    });
+}
+    
+
+    
+
+$( document ).ready(function() {
+  // Handler for .ready() called.
+   $("#btnLoginSubmit").click(displayWithAjax);
+
+   // $("#btnLoginSubmit").click(displayWithPromise);
+    //$("#searchIcon").click()
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/js/search.js
@@ -0,0 +1,84 @@
+
+
+function search() {
+    var obj = { };
+    $.ajax({
+        url: "http://localhost:49980/api/Authentication/Search?searchTerm=Test",
+      
+        type: "GET",
+        data: {Name, Description, ImageId, Price}
+    
+        
+    }) 
+}
+$(document).ready(function() {
+$('#autocomplete').autocomplete({
+    source: function (request, response) {
+        
+        $.getJSON("http://localhost:49980/api/Authentication/Search?searchTerm=" + request.term, function (data) {
+            response($.map(data, function (value, key) {
+               
+                return {
+                    label: value.Name,
+                    value: value.Name,
+                    object: value
+                };
+            }));
+        });
+    },
+    select: function( event, ui ) {
+        obj = ui.item.object;
+        displayItem(obj);
+    },
+    
+    minLength: 2,
+    delay: 100
+    
+});
+
+});
+
+function displayItem(obj){
+     console.log(obj);
+    $("#itemName").html(obj.Name);
+    $("#itemDescription").html(obj.Description);
+    $("#itemPrice").html(obj.Price);
+    var image = obj.ImageId + '.jpg';
+   // var image = "xyz" + '.jpg';
+    var imageUrl = "../img/"+image;
+    $("#carImg").attr("src", imageUrl );
+
+}
+    
+
+
+
+function displayWithAjax() {
+    var obj = {};
+    obj.userName = $("#userName").val();
+    obj.password = $("#password").val();
+   var obj = JSON.stringify(obj);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:49980/api/Authentication/ValidateUser",
+        data: obj,
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8", 
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            if(data.ValidUser === true && data.Reason === "Validated"){
+            window.location.href = "/myPage.html"
+            }
+            else {
+                $("#errorMsgText").text("Invalid user name or password");
+            }    
+            
+        },
+        error: function () {
+            alert("error");
+           
+        }
+    });
+}
+    
